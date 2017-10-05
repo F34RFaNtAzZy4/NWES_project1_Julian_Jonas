ac5cf47aa82597182045a2b7a3aa7edf87bb6842
F34RFaNtAzZy4 <f34rfantazzy4@gmail.com>
2017-10-05 15:27:33 +0200

    initial commit

---
+++ NWES_project1_Julian_Jonas/main.c
@@ -0,0 +1,18 @@
+/*
+ * NWES_project1_Julian_Jonas.c
+ *
+ * Created: 05.10.2017 15:26:47
+ * Author : jonas
+ */ 
+
+#include <avr/io.h>
+
+
+int main(void)
+{
+    /* Replace with your application code */
+    while (1) 
+    {
+    }
+}
+
